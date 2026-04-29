e5cd463be76c02c97e001966c82327c8ac5a8b1b
Xanderzoid <randolph.alpha@gmail.com>
2026-04-28 23:23:50 -1000

    Initial commit: Odd or Even, empty tests

---
+++ src/project_02/script_project_02.py
@@ -0,0 +1,41 @@
+"""
+Odd or Even
+-----------
+A CLI utility to determine if a users input number is odd or even.
+"""
+
+
+def result_msg(is_even: bool, num: float) -> str:
+    if is_even:
+        return f"The number {num} is even.\n"
+    else:
+        return f"The number {num} is odd.\n"
+
+
+def validate_input() -> float:
+    msg = "This CLI app checks if a user's number is odd or even."
+    spacer = "-" * len(msg)
+    print(f"{spacer}\n{msg}\n{spacer}")
+    while True:
+        raw_input = input("Enter your number: ").replace(" ", "")
+        try:
+            num = float(raw_input)
+            return num
+
+        except ValueError:
+            print(f"Invalid input '{raw_input}': Enter a digit.")
+
+
+def identify_even(num: float) -> bool:
+    return num % 2 == 0
+
+
+def run_app() -> None:
+    num = validate_input()
+    is_even = identify_even(num)
+    result_str = result_msg(is_even, num)
+    print(result_str)
+
+
+if __name__ == "__main__":
+    run_app()
